5c161f5a3cfc453f06e22fbef76462e562eb63f5
高旭甫 <formerly000@gmail.com>
2025-07-23 13:50:51 +0000

    modify

--- lesson9/index.py
+++ lesson9/index.py
@@ -1,4 +1,4 @@
-import tools
+import box
 #from tools import play_game,Empty(class)
 #可用from ... import ...讀取指定的function和class
 
@@ -8,12 +8,12 @@ def main():
         play_count = 0
         while(True):
             play_count += 1
-            tools.play_game() #要記得是執行tools裡的play_game
+            box.play_game() #要記得是執行box裡的play_game
             is_continue = input("您還要繼續嗎(y,n)?")
             if is_continue == "n":
                 break
             
-        print(f"{tools.use_name}共玩了{play_count}次") #把tools的常數帶進來
+        print(f"{box.use_name}共玩了{play_count}次") #把tools的常數帶進來
         print("遊戲結束")
     except ValueError:
         print("格式錯誤")
